9ceadfaf5dee6527dc795295bf8e59a2fc2e78d8
tabascoeye <tabascoeye@gmail.com>
2021-12-24 12:42:50 +0000

    add more companions

--- front/src/Phaser/Companion/CompanionTextures.ts
+++ front/src/Phaser/Companion/CompanionTextures.ts
@@ -11,4 +11,8 @@ export const COMPANION_RESOURCES: CompanionResourceDescriptionInterface[] = [
     { name: "cat1", img: "resources/characters/pipoya/Cat 01-1.png", behaviour: "cat" },
     { name: "cat2", img: "resources/characters/pipoya/Cat 01-2.png", behaviour: "cat" },
     { name: "cat3", img: "resources/characters/pipoya/Cat 01-3.png", behaviour: "cat" },
+    { name: "Drone", img: "resources/companions/Drone.png", behaviour: "cat" },
+    { name: "Penguin", img: "resources/companions/Penguin.png", behaviour: "cat" },
+    { name: "RobotVacuum", img: "resources/companions/RobotVacuum.png", behaviour: "cat" },
+    { name: "Roomba", img: "resources/companions/Roomba.png", behaviour: "cat" },
 ];
