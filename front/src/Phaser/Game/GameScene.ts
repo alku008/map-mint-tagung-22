807e4a1717ce7e18d941d9cc71910844e93aad14
PizZaKatZe <pizzakatze@chaos-siegen.de>
2021-12-28 17:48:31 +0100

    Add setting for blocking external content

--- front/src/Phaser/Game/GameScene.ts
+++ front/src/Phaser/Game/GameScene.ts
@@ -518,7 +518,7 @@ export class GameScene extends DirtyScene {
                     if (object.text) {
                         TextUtils.createTextFromITiledMapObject(this, object);
                     }
-                    if (object.type === "website") {
+                    if (object.type === "website" && !localUserStore.getBlockExternalContent()) {
                         // Let's load iframes in the map
                         const url = PropertyUtils.mustFindStringProperty(
                             GameMapProperties.URL,
@@ -787,6 +787,9 @@ export class GameScene extends DirtyScene {
                  * Triggered when we receive the JWT token to connect to Jitsi
                  */
                 this.connection.onStartJitsiRoom((jwt, room) => {
+                    if (localUserStore.getBlockExternalContent()) {
+                        return;
+                    }
                     this.startJitsi(room, jwt);
                 });
 
@@ -938,6 +941,9 @@ export class GameScene extends DirtyScene {
         });
 
         this.gameMap.onPropertyChange(GameMapProperties.JITSI_ROOM, (newValue, oldValue, allProps) => {
+            if (localUserStore.getBlockExternalContent()) {
+                return;
+            }
             if (newValue === undefined) {
                 layoutManagerActionStore.removeAction("jitsi");
                 this.stopJitsi();
